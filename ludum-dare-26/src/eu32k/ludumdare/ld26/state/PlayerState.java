db322e64aeb5c3f7468e72d6c3e22b3029839a9b
Andreas Doebeli <thobens@gmail.com>
2013-04-27 18:39:28 +0200

    added new (pseudo-) states, implemented tile shifting

---
+++ ludum-dare-26/src/eu32k/ludumdare/ld26/state/PlayerState.java
@@ -0,0 +1,23 @@
+package eu32k.ludumdare.ld26.state;
+
+public class PlayerState extends GameState {
+
+   @Override
+   public void init() {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void enter() {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void destroy() {
+      // TODO Auto-generated method stub
+
+   }
+
+}
