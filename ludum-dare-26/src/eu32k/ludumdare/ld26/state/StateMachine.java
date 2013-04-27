0b0917458f841c8f14feb9d68a4b82b4af54bc32
Andreas Doebeli <andreas.doebeli@trapadoo.com>
2013-04-27 15:27:06 +0200

    state machine improved

--- ludum-dare-26/src/eu32k/ludumdare/ld26/state/StateMachine.java
+++ ludum-dare-26/src/eu32k/ludumdare/ld26/state/StateMachine.java
@@ -1,35 +1,55 @@
 package eu32k.ludumdare.ld26.state;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StateMachine {
-	
+
 	private static StateMachine INSTANCE;
-	
-	private List<GameState> states;
-	
+
+	private Map<Class<? extends GameState>, GameState> states;
+
+	private GameState current;
+
 	private StateMachine() {
-		states = new ArrayList<GameState>();
+		states = new HashMap<Class<? extends GameState>, GameState>();
 	}
-	
+
 	public StateMachine instance() {
-		if(INSTANCE == null) {
+		if (INSTANCE == null) {
 			INSTANCE = new StateMachine();
 		}
 		return INSTANCE;
 	}
-	
-	public GameState createState(Class<? extends GameState> stateClass) {
+
+	@SuppressWarnings("unchecked")
+	public <T extends GameState> T createState(Class<T> stateClass) {
+		GameState state = states.get(stateClass);
+		if(state != null) {
+			state.destroy();
+		}
 		try {
-			GameState state = stateClass.getConstructor().newInstance();
+			state = stateClass.getConstructor().newInstance();
 			state.init();
-			this.states.add(state);
-			return state;
+			this.states.put(stateClass, state);
+			return (T) state;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T extends GameState> T enterState(Class<T> stateClass) {
+		if (current.getTransitions().contains(stateClass)) {
+			current = states.get(stateClass);
+			if (current != null) {
+				current.enter();
+			}
+		} else {
+			System.out.println("State has no transition to "+stateClass.getName());
+		}
+		return (T) current;
 	}
 
 }
