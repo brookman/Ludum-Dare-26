eb3fe7b7884b0044094e3a0af74627ebef287b74
brookman <rc-p90@bluemail.ch>
2013-04-27 18:54:43 +0200

    Player can now move diagonally

--- ludum-dare-26/src/eu32k/ludumdare/ld26/Player.java
+++ ludum-dare-26/src/eu32k/ludumdare/ld26/Player.java
@@ -1,54 +1,75 @@
 package eu32k.ludumdare.ld26;
 
+import java.util.List;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import eu32k.ludumdare.ld26.util.CollisionUtil;
 
-import java.util.List;
+import eu32k.ludumdare.ld26.util.CollisionUtil;
 
 public class Player extends GameObject {
 
-    public static final float WIDTH = 4, HEIGHT = 4;
-    public static final float SPEED = 30;
+   public static final float WIDTH = 4, HEIGHT = 4;
+   public static final float SPEED = 30;
 
+   public Player(int x, int y) {
+      super(new Vector2(x, y), WIDTH, HEIGHT);
 
-    public Player(int x, int y) {
-        super(new Vector2(x, y), WIDTH, HEIGHT);
+      bounds.add(new Rectangle(x - WIDTH / 2, y - HEIGHT / 2, WIDTH, HEIGHT));
+   }
 
-        bounds.add(new Rectangle(x - WIDTH / 2, y - HEIGHT / 2, WIDTH, HEIGHT));
-    }
+   @Override
+   public Sprite loadSprite() {
+      Texture texture = new Texture(Gdx.files.internal("textures/shroom.png"));
+      Sprite sprite = new Sprite(texture);
+      sprite.setSize(WIDTH, HEIGHT);
+      sprite.setPosition(position.x, position.y);
 
-    @Override
-    public Sprite loadSprite() {
-        Texture texture = new Texture(Gdx.files.internal("textures/shroom.png"));
-        Sprite sprite = new Sprite(texture);
-        sprite.setSize(WIDTH, HEIGHT);
-        sprite.setPosition(position.x, position.y);
+      return sprite;
+   }
 
-        return sprite;
-    }
+   public void update(List<Tile2> tiles) {
+      super.update();
 
-    public void update(List<Tile2> tiles) {
-        super.update();
+      for (Tile2 tile : tiles) {
+         for (Rectangle tileBound : tile.getBounds()) {
+            for (Rectangle playerBound : bounds) {
+               if (CollisionUtil.rectanglesOverlap(tileBound, playerBound)) {
 
-        for (Tile2 tile : tiles) {
-            for (Rectangle tileBound : tile.getBounds()) {
-                for (Rectangle playerBound : bounds) {
-                    if (CollisionUtil.rectanglesOverlap(tileBound, playerBound)) {
-                        
-                        System.out.println("COLLISION");
-                    }
-                }
+                  System.out.println("COLLISION");
+               }
             }
-        }
-    }
+         }
+      }
+   }
 
-    @Override
-    public float getSpeed() {
-        return SPEED;
-    }
+   @Override
+   public void move(Vector2 velocity) {
+      super.move(velocity.mul(SPEED));
+   }
+
+   // public void move(Vector2 velocity, List<Tile2> tiles) {
+   // velocity.mul(SPEED);
+   // Vector2 nextPos = position.cpy().add(velocity);
+   //
+   // for (Tile2 tile : tiles) {
+   // for (Rectangle tileBound : tile.getBounds()) {
+   // for (Rectangle playerBound : bounds) {
+   // if (CollisionUtil.rectanglesOverlap(tileBound, playerBound)) {
+   //
+   // System.out.println("COLLISION");
+   // }
+   // }
+   // }
+   // }
+   // }
+
+   @Override
+   public float getSpeed() {
+      return SPEED;
+   }
 
 }
