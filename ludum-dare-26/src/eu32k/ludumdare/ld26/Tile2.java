553d9b344a22af6a44db1fb1762ee053b164d512
atombrot@gmail.com <atombrot@gmail.com>
2013-04-27 19:24:31 +0200

    Tile2: Added getSprite()

--- ludum-dare-26/src/eu32k/ludumdare/ld26/Tile2.java
+++ ludum-dare-26/src/eu32k/ludumdare/ld26/Tile2.java
@@ -12,116 +12,119 @@ import java.util.List;
 
 public class Tile2 {
 
-    public void draw(SpriteBatch batch) {
-        sprite.draw(batch);
-    }
+   public void draw(SpriteBatch batch) {
+      sprite.draw(batch);
+   }
 
-    enum Type {
-        L, I, X, T // L Shape, I Shape, X Shape (+ Shape), T Shape
-    }
+   public Sprite getSprite() {
+      return sprite;
+   }
 
-    enum Rotation {
-        R, L, U, D // Right, Left, Up, Down
-    }
+   enum Type {
+      L, I, X, T // L Shape, I Shape, X Shape (+ Shape), T Shape
+   }
 
-    private Type type;
-    private Rotation rotation;
-    private int x, y;
-    public static final float WIDTH = 27, HEIGHT = 27;
-    private Sprite sprite;
-    private List<Rectangle> bounds;
+   enum Rotation {
+      R, L, U, D // Right, Left, Up, Down
+   }
 
-    public Tile2(int x, int y, Type type, Rotation rotation) {
-        this.type = type;
-        this.rotation = rotation;
-        this.x = x;
-        this.y = y;
-        this.sprite = loadSprite();
-        this.bounds = calculateBounds();
-    }
+   private Type type;
+   private Rotation rotation;
+   private int x, y;
+   public static final float WIDTH = 27, HEIGHT = 27;
+   private Sprite sprite;
+   private List<Rectangle> bounds;
 
-    public List<Rectangle> getBounds() {
-        return bounds;
-    }
+   public Tile2(int x, int y, Type type, Rotation rotation) {
+      this.type = type;
+      this.rotation = rotation;
+      this.x = x;
+      this.y = y;
+      this.sprite = loadSprite();
+      this.bounds = calculateBounds();
+   }
 
-    private List<Rectangle> calculateBounds() {
-        List<Rectangle> tmp = new ArrayList<Rectangle>();
+   public List<Rectangle> getBounds() {
+      return bounds;
+   }
 
-        switch (type) {
-            case I:
-                tmp.add(new Rectangle(x + 0, y + 0, 9, 9));
-                tmp.add(new Rectangle(x + 0, y + 9, 9, 9));
-                tmp.add(new Rectangle(x + 0, y + 18, 9, 9));
-                tmp.add(new Rectangle(x + 18, y + 0, 9, 9));
-                tmp.add(new Rectangle(x + 18, y + 9, 9, 9));
-                tmp.add(new Rectangle(x + 18, y + 18, 9, 9));
-                break;
-            case L:
-                tmp.add(new Rectangle());
-                break;
-            case T:
-                tmp.add(new Rectangle());
-                break;
-            case X:
-                tmp.add(new Rectangle());
-                break;
-            default:
-                tmp.add(new Rectangle());
-        }
+   private List<Rectangle> calculateBounds() {
+      List<Rectangle> tmp = new ArrayList<Rectangle>();
 
-        //todo rotate rectangles depending on rotation/orientation
+      switch (type) {
+      case I:
+         tmp.add(new Rectangle(x + 0, y + 0, 9, 9));
+         tmp.add(new Rectangle(x + 0, y + 9, 9, 9));
+         tmp.add(new Rectangle(x + 0, y + 18, 9, 9));
+         tmp.add(new Rectangle(x + 18, y + 0, 9, 9));
+         tmp.add(new Rectangle(x + 18, y + 9, 9, 9));
+         tmp.add(new Rectangle(x + 18, y + 18, 9, 9));
+         break;
+      case L:
+         tmp.add(new Rectangle());
+         break;
+      case T:
+         tmp.add(new Rectangle());
+         break;
+      case X:
+         tmp.add(new Rectangle());
+         break;
+      default:
+         tmp.add(new Rectangle());
+      }
 
-        return tmp;
-    }
+      // todo rotate rectangles depending on rotation/orientation
 
-    public Sprite loadSprite() {
-        Sprite sprite;
-        TextureRegion textureRegion;
-        Texture texture = new Texture(Gdx.files.internal("textures/tiles_round.png"));
+      return tmp;
+   }
 
-        switch (type) {
-            case I:
-                textureRegion = new TextureRegion(texture, 0, 27, 27, 27);
-                break;
-            case L:
-                textureRegion = new TextureRegion(texture, 54, 27, 27, 27);
-                break;
-            case T:
-                textureRegion = new TextureRegion(texture, 27, 54, 27, 27);
-                break;
-            case X:
-                textureRegion = new TextureRegion(texture, 0, 54, 27, 27);
-                break;
-            default:
-                textureRegion = new TextureRegion(); //todo
-        }
-        sprite = new Sprite(textureRegion);
-        sprite.setSize(WIDTH, HEIGHT);
-        sprite.setPosition(x, y);
+   public Sprite loadSprite() {
+      Sprite sprite;
+      TextureRegion textureRegion;
+      Texture texture = new Texture(Gdx.files.internal("textures/tiles_round.png"));
 
-        switch (rotation) {
-            case D:
-                sprite.rotate(0);
-                break;
-            case L:
-                sprite.rotate(90);
-                break;
-            case R:
-                sprite.rotate(180);
-                break;
-            case U:
-                sprite.rotate(270);
-                break;
-            default:
-                //todo
-        }
+      switch (type) {
+      case I:
+         textureRegion = new TextureRegion(texture, 0, 27, 27, 27);
+         break;
+      case L:
+         textureRegion = new TextureRegion(texture, 54, 27, 27, 27);
+         break;
+      case T:
+         textureRegion = new TextureRegion(texture, 27, 54, 27, 27);
+         break;
+      case X:
+         textureRegion = new TextureRegion(texture, 0, 54, 27, 27);
+         break;
+      default:
+         textureRegion = new TextureRegion(); // todo
+      }
+      sprite = new Sprite(textureRegion);
+      sprite.setSize(WIDTH, HEIGHT);
+      sprite.setPosition(x, y);
 
-        return sprite;
-    }
+      switch (rotation) {
+      case D:
+         sprite.rotate(0);
+         break;
+      case L:
+         sprite.rotate(90);
+         break;
+      case R:
+         sprite.rotate(180);
+         break;
+      case U:
+         sprite.rotate(270);
+         break;
+      default:
+         // todo
+      }
 
-    private void rotate(Rectangle rectangle) {
-        //todo
-    }
+      return sprite;
+   }
 
+   private void rotate(Rectangle rectangle) {
+      // todo
+   }
 
 }
