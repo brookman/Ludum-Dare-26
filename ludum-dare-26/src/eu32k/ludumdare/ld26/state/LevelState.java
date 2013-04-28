a12182a9e6011769b3443f1990b6206ee6737c0d
atombrot@gmail.com <atombrot@gmail.com>
2013-04-28 16:07:28 +0200

    Integrated tile move test into game :D

--- ludum-dare-26/src/eu32k/ludumdare/ld26/state/LevelState.java
+++ ludum-dare-26/src/eu32k/ludumdare/ld26/state/LevelState.java
@@ -1,27 +1,36 @@
 package eu32k.ludumdare.ld26.state;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import eu32k.ludumdare.ld26.level.Level;
+import eu32k.ludumdare.ld26.level.TileMove;
 
 public class LevelState extends GameState {
-   
+
    private Level level;
+   private List<TileMove> movingTiles;
 
-	@Override
-	public void init() {
-		transitions.add(LevelFinishedState.class);
-	}
+   public LevelState() {
+      this.movingTiles = new ArrayList<TileMove>();
+   }
 
-	@Override
-	public void enter() {
-		// TODO Auto-generated method stub
+   @Override
+   public void init() {
+      transitions.add(LevelFinishedState.class);
+   }
 
-	}
+   @Override
+   public void enter() {
+      // TODO Auto-generated method stub
 
-	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
+   }
 
-	}
+   @Override
+   public void destroy() {
+      // TODO Auto-generated method stub
+
+   }
 
    public Level getLevel() {
       return level;
@@ -31,4 +40,8 @@ public class LevelState extends GameState {
       this.level = level;
    }
 
+   public List<TileMove> getMovingTiles()
+   {
+      return movingTiles;
+   }
 }
