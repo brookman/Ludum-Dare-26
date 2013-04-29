49beb8b8c49c0d6af928d46d026244c3ee151c9c
atombrot@gmail.com <atombrot@gmail.com>
2013-04-29 11:13:34 +0200

    Added param to game event (can be used to further specify parameters for the event)

--- ludum-dare-26/src/eu32k/ludumdare/ld26/gameplay/GameplayEvent.java
+++ ludum-dare-26/src/eu32k/ludumdare/ld26/gameplay/GameplayEvent.java
@@ -4,15 +4,28 @@ import eu32k.ludumdare.ld26.events.EventBase;
 
 public class GameplayEvent extends EventBase {
    
+   public final static int PARAM_NONE = 0;
+   
+   public final static int PARAM_LOSE_FALLOFFBOARD = 1;
+   public final static int PARAM_LOSE_TIMEOUT = 2;
+   public final static int PARAM_LOSE_SQUASHED = 3;
+   
    public enum GameplayEventType {
       WIN,LOSE;
    }
-   
+      
    private GameplayEventType type;
+   private int param;
    
    public GameplayEvent(GameplayEventType type) {
-      this.type = type;
+      this(type, PARAM_NONE);
    }
+
+   public GameplayEvent(GameplayEventType type, int param) {
+      this.type = type;
+      this.setParam(param);
+   }
+   
 
    public GameplayEventType getType() {
       return type;
@@ -20,6 +33,14 @@ public class GameplayEvent extends EventBase {
 
    public void setType(GameplayEventType type) {
       this.type = type;
+   }
+
+   public int getParam() {
+      return param;
+   }
+
+   public void setParam(int param) {
+      this.param = param;
    }
    
 }
