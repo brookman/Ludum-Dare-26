5d94a2800a7cc37ec1ade29c5100897dcc910bfe
brookman <rc-p90@bluemail.ch>
2013-04-30 00:10:22 +0200

    Added seed mode settings

--- ludum-dare-26/src/eu32k/ludumdare/ld26/stages/MenuStage.java
+++ ludum-dare-26/src/eu32k/ludumdare/ld26/stages/MenuStage.java
@@ -8,9 +8,12 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldFilter;
 
 import eu32k.libgdx.rendering.Textures;
 import eu32k.ludumdare.ld26.effects.EffectsManager;
@@ -30,6 +33,8 @@ public class MenuStage extends Stage {
    private TextButton challengeButton;
    private TextButton seedButton;
    private TextButton exitButton;
+   private SelectBox size;
+   private TextField seed;
 
    public MenuStage(EffectsManager effects) {
       this.effects = effects;
@@ -47,7 +52,7 @@ public class MenuStage extends Stage {
       challengeButton.addListener(new InputListener() {
          @Override
          public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-            //TODO: Move this shit into levelstate
+            // TODO: Move this shit into levelstate
             LevelState levelState = StateMachine.instance().getState(LevelState.class);
             LevelConfigSequence levels = new LevelConfigSequence();
             LevelConfigSequence.addLevelsToSequence(levels, 92309, 3, 3, 12, 8, 25);
@@ -64,7 +69,8 @@ public class MenuStage extends Stage {
       seedButton.addListener(new InputListener() {
          @Override
          public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-
+            long seedValue = Long.parseLong(seed.getText());
+            System.out.println("start seed mode with seed: " + seedValue);
             return false;
          }
       });
@@ -79,15 +85,31 @@ public class MenuStage extends Stage {
          }
       });
 
+      size = new SelectBox(new String[] { "3x3", "4x4", "5x5" }, skin);
+      seed = new TextField("31337", skin);
+      seed.setTextFieldFilter(new TextFieldFilter() {
+
+         @Override
+         public boolean acceptChar(TextField textField, char key) {
+            int code = key;
+            return code >= 48 && code <= 57;
+         }
+      });
+
       int padding = 4;
 
-      table.add(title).fill().pad(padding);
+      table.add(title).fill().pad(padding).colspan(3);
       table.row();
-      table.add(challengeButton).fill().pad(padding);
+      table.add(challengeButton).fill().pad(padding).colspan(3);
       table.row();
+
+      table.add(size).fill().pad(padding);
+      table.add(seed).fill().pad(padding);
       table.add(seedButton).fill().pad(padding);
+
       table.row();
-      table.add(exitButton).fill().pad(padding);
+      table.add(exitButton).fill().pad(padding).colspan(3);
+
       table.row();
 
       addActor(table);
@@ -100,6 +122,9 @@ public class MenuStage extends Stage {
       seedButton.setColor(color);
       exitButton.setColor(color);
       title.setColor(color);
+      size.setColor(color);
+      seed.setColor(color);
+      seed.getStyle().fontColor.a = color.a;
 
       Background.getInstance().draw(new Vector3(color.r, color.g, color.b), false);
       super.draw();
