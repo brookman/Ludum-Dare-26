3f45d015257d5b99713a84a679dd8299f58abb5e
brookman <rc-p90@bluemail.ch>
2013-04-30 00:38:50 +0200

    Updated seed mode settings in menu

--- ludum-dare-26/src/eu32k/ludumdare/ld26/stages/MenuStage.java
+++ ludum-dare-26/src/eu32k/ludumdare/ld26/stages/MenuStage.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -20,7 +19,6 @@ import eu32k.libgdx.rendering.Textures;
 import eu32k.ludumdare.ld26.effects.EffectsManager;
 import eu32k.ludumdare.ld26.level.LevelConfigSequence;
 import eu32k.ludumdare.ld26.rendering.Background;
-import eu32k.ludumdare.ld26.state.LevelInitState;
 import eu32k.ludumdare.ld26.state.LevelState;
 import eu32k.ludumdare.ld26.state.StateMachine;
 
@@ -34,7 +32,7 @@ public class MenuStage extends Stage {
    private TextButton challengeButton;
    private TextButton seedButton;
    private TextButton exitButton;
-   private SelectBox size;
+   private TextField size;
    private TextField seed;
 
    public MenuStage(EffectsManager effects) {
@@ -49,30 +47,33 @@ public class MenuStage extends Stage {
       title = new Image(new TextureRegion(Textures.get("textures/title.png"), 256, 64));
 
       challengeButton = new TextButton("Challenge mode", skin);
-      challengeButton.setColor(Color.CYAN);
       challengeButton.addListener(new InputListener() {
          @Override
          public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-            //TODO: Move this shit into levelstate
+            // TODO: Move this shit into levelstate
             challengeMode();
             return false;
          }
       });
 
       seedButton = new TextButton("Seed mode", skin);
-      seedButton.setColor(Color.CYAN);
       seedButton.addListener(new InputListener() {
          @Override
          public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
             seedMode();
-            long seedValue = Long.parseLong(seed.getText());
-            System.out.println("start seed mode with seed: " + seedValue);
+            try {
+               long seedValue = Long.parseLong(seed.getText());
+               int width = Integer.parseInt(size.getText().split("x")[0]);
+               int height = Integer.parseInt(size.getText().split("x")[1]);
+               System.out.println("start seed mode " + width + "x" + height + " with seed " + seedValue);
+            } catch (Exception e) {
+               // ignore
+            }
             return false;
          }
       });
 
       exitButton = new TextButton("Exit", skin);
-      exitButton.setColor(Color.CYAN);
       exitButton.addListener(new InputListener() {
          @Override
          public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -81,10 +82,23 @@ public class MenuStage extends Stage {
          }
       });
 
-      size = new SelectBox(new String[] { "3x3", "4x4", "5x5" }, skin);
-      seed = new TextField("31337", skin);
-      seed.setTextFieldFilter(new TextFieldFilter() {
+      size = new TextField("3x3", skin) {
+         @Override
+         public float getPrefWidth() {
+            return 50;
+         }
 
+      };
+
+      seed = new TextField("31337", skin) {
+         @Override
+         public float getPrefWidth() {
+            return 100;
+         }
+
+      };
+
+      seed.setTextFieldFilter(new TextFieldFilter() {
          @Override
          public boolean acceptChar(TextField textField, char key) {
             int code = key;
@@ -113,9 +127,9 @@ public class MenuStage extends Stage {
 
    @Override
    public void draw() {
-      if(Gdx.input.isKeyPressed(Input.Keys.C)) {
+      if (Gdx.input.isKeyPressed(Input.Keys.C)) {
          challengeMode();
-      } else if(Gdx.input.isKeyPressed(Input.Keys.S)) {
+      } else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
          seedMode();
       }
       Color color = effects.getCurrentColor();
@@ -133,7 +147,7 @@ public class MenuStage extends Stage {
 
    private void seedMode() {
       // TODO Auto-generated method stub
-      
+
    }
 
    private void challengeMode() {
