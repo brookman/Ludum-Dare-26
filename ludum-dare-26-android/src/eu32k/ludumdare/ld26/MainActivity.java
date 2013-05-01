e04f89fb342404c20f53b56db756b98184f0cf2a
atombrot@gmail.com <atombrot@gmail.com>
2013-05-01 20:42:40 +0200

    added import

--- ludum-dare-26-android/src/eu32k/ludumdare/ld26/MainActivity.java
+++ ludum-dare-26-android/src/eu32k/ludumdare/ld26/MainActivity.java
@@ -1,5 +1,7 @@
 package eu32k.ludumdare.ld26;
 
+import android.os.Bundle;
+
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
