7b4da75ec0dd203bd88604eb43c2e7cc68863fe9
atombrot@gmail.com <atombrot@gmail.com>
2013-05-06 01:29:17 +0200

    removed obsolete methods

--- ludum-dare-26/src/eu32k/ludumdare/ld26/level/Level.java
+++ ludum-dare-26/src/eu32k/ludumdare/ld26/level/Level.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import java.util.Set;
 
 import eu32k.ludumdare.ld26.Direction;
 import eu32k.ludumdare.ld26.level.Tile.Rotation;
@@ -99,30 +98,6 @@ public class Level {
       }
    }
 
-   public boolean isHorizontalNearGameobject(GameObject object, Tile tile, int distance) {
-      if (object == null || tile == null)
-         return false;
-      if (distance < 0)
-         distance = 0;
-      int ox = (int) object.getX();
-      int tx = (int) tile.getX();
-
-      int xDistance = Math.abs(ox - tx);
-      return (xDistance <= distance);
-   }
-
-   public boolean isVerticalNearGameobject(GameObject object, Tile tile, int distance) {
-      if (object == null || tile == null)
-         return false;
-      if (distance < 0)
-         distance = 0;
-      int oy = (int) object.getY();
-      int ty = (int) tile.getY();
-
-      int yDistance = Math.abs(oy - ty);
-      return (yDistance <= distance);
-   }
-
    public Tile spawnTile() {
       LevelState ls = StateMachine.instance().getState(LevelState.class);
       PlayerState ps = StateMachine.instance().getState(PlayerState.class);
@@ -141,7 +116,6 @@ public class Level {
       do {
          target = candidates.get(tileRandom.nextInt(candidates.size()));
          candidates.remove(target);
-         Map<Direction, Tile> n = target.getNeighbors();
          boolean foundValid = false;
          availableDirections = getAvailableDirections(target);
          if(getXDistance(player, target) <= spawnDistance)
@@ -227,123 +201,7 @@ public class Level {
       return list;
    }
 
-   private boolean checkVerticalDirection(Player player, Goal goal, Tile target, Direction targetDir) {
-      return (targetDir == Direction.N || targetDir == Direction.S) && (!isVerticalNearGameobject(player, target, spawnDistance) && !isVerticalNearGameobject(goal, target, spawnDistance));
-   }
 
-   private boolean checkHorizontalDirection(Player player, Goal goal, Tile target, Direction targetDir) {
-      return (targetDir == Direction.E || targetDir == Direction.W) && (!isHorizontalNearGameobject(player, target, spawnDistance) && !isHorizontalNearGameobject(goal, target, spawnDistance));
-   }
-
-   private boolean checkTileAndGoal(Player player, Goal goal, Tile t) {
-      return isHorizontalNearGameobject(player, t, spawnDistance) || isHorizontalNearGameobject(goal, t, spawnDistance) || isVerticalNearGameobject(goal, t, spawnDistance)
-            || isVerticalNearGameobject(player, t, spawnDistance);
-   }
-
-   public Tile spawnTileOld() {
-      List<Tile> edgeTiles = getEdgeTiles();
-      int randomTile = tileRandom.nextInt(edgeTiles.size());
-
-      Tile target = edgeTiles.get(randomTile);
-      float xRand = target.getX();
-      float yRand = target.getY();
-      Set<Direction> dirs = target.getNeighbors().keySet();
-      List<Direction> freeDirs = new ArrayList<Direction>();
-      LevelState ls = StateMachine.instance().getState(LevelState.class);
-      PlayerState ps = StateMachine.instance().getState(PlayerState.class);
-      System.out.println("Player: " + ps.getPlayer().getX() + "/" + ps.getPlayer().getY() + " Object: " + ls.getGoal().getX() + "/" + ls.getGoal().getY());
-      System.out.println("Tile: " + target.getX() + "/" + target.getY());
-      System.out.println("Tile Neighbours:");
-      for (Direction key : target.getNeighbors().keySet()) {
-         System.out.println(key);
-      }
-      if (dirs.size() == 3) {
-         freeDirs.add(Direction.N);
-         freeDirs.add(Direction.E);
-         freeDirs.add(Direction.S);
-         freeDirs.add(Direction.W);
-         for (Direction dir : dirs) {
-            freeDirs.remove(dir);
-         }
-      } else {
-         if (isHorizontalNearGameobject(ps.getPlayer(), target, spawnDistance) || isHorizontalNearGameobject(ls.getGoal(), target, spawnDistance)) {
-            if (!dirs.contains(Direction.E)) {
-               freeDirs.add(Direction.E);
-            }
-            if (!dirs.contains(Direction.W)) {
-               freeDirs.add(Direction.W);
-            }
-         }
-         if (isVerticalNearGameobject(ps.getPlayer(), target, spawnDistance) || isVerticalNearGameobject(ls.getGoal(), target, spawnDistance)) {
-            if (!dirs.contains(Direction.getOpposite(Direction.N))) {
-               freeDirs.add(Direction.N);
-            }
-            if (!dirs.contains(Direction.S)) {
-               freeDirs.add(Direction.S);
-            }
-         }
-      }
-      System.out.println("Free Dirs:");
-      printDirs(freeDirs);
-      int size = freeDirs.size();
-      Direction dir = size <= 1 ? freeDirs.get(0) : freeDirs.get(tileRandom.nextInt(size));
-      float x, y;
-      switch (dir) {
-      case S:
-         x = xRand;
-         y = yRand - Tile.SIZE;
-         break;
-      case W:
-         x = xRand - Tile.SIZE;
-         y = yRand;
-         break;
-      case N:
-         x = xRand;
-         y = yRand + Tile.SIZE;
-         break;
-      case E:
-      default:
-         x = xRand + Tile.SIZE;
-         y = yRand;
-         break;
-      }
-      Tile nextTile = createRandomTile(x, y);
-      nextTile.getNeighbors().put(Direction.getOpposite(dir), target);
-      target.getNeighbors().put(dir, nextTile);
-      // tiles.add(nextTile);
-      return nextTile;
-   }
-
-   private void printDirs(List<Direction> blabla) {
-      for (Direction dir : blabla) {
-         System.out.println(blabla);
-      }
-   }
-
-   private List<Tile> getEdgeTiles() {
-      Player player = StateMachine.instance().getState(PlayerState.class).getPlayer();
-      Goal goal = StateMachine.instance().getState(LevelState.class).getGoal();
-      List<Tile> edgeTiles = new ArrayList<Tile>();
-      for (Tile tile : pool.items()) {
-         if (tile.isInUse()) {
-            Map<Direction, Tile> neighbours = tile.getNeighbors();
-            if (((!neighbours.containsKey(Direction.N) || !neighbours.containsKey(Direction.S)) && (isVerticalNearGameobject(player, tile, spawnDistance) || isHorizontalNearGameobject(goal, tile,
-                  spawnDistance)))
-                  || ((!neighbours.containsKey(Direction.E) || !neighbours.containsKey(Direction.W)) && (isHorizontalNearGameobject(player, tile, spawnDistance) || isHorizontalNearGameobject(goal,
-                        tile, spawnDistance)))) {
-               edgeTiles.add(tile);
-            }
-         }
-      }
-      if (edgeTiles.size() == 0) {
-         for (Tile tile : pool.items()) {
-            if (tile.isInUse() && tile.getNeighbors().size() < 4) {
-               edgeTiles.add(tile);
-            }
-         }
-      }
-      return edgeTiles;
-   }
 
    public void popTile(Tile tile) {
       tile.setInUse(false);
